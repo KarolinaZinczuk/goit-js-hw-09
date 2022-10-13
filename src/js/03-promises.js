e74cfbcf99788399251f0b45cbe13b478733259c
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2022-10-13 09:42:51 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,12 +6,8 @@ const delayStep = document.querySelector('[name="step"]');
 const amount = document.querySelector('[name="amount"]');
 // const submitBtn = document.querySelector('[type="submit"]');
 
-let valueDelay = Number(firstDelay.value);
-let valueStep = Number(delayStep.value);
-let valueAmount = Number(amount.value);
-
 function createPromise(position, delay) {
-  const promise = new Promise ((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
@@ -25,14 +21,19 @@ function createPromise(position, delay) {
   promise
   .then(({position, delay}) => { 
   Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-})
-.catch(({position, delay}) => {
+  })
+  .catch(({position, delay}) => {
   Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-});
+  });
 }
 
 function sendSubmit(event) {
   event.preventDefault();
+  
+  let valueDelay = Number(firstDelay.value);
+  let valueStep = Number(delayStep.value);
+  let valueAmount = Number(amount.value);
+  
   for (let i =0; i < valueAmount; i++) {
     createPromise(i, valueDelay);
     valueDelay += valueStep;
