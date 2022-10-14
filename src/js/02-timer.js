474c51bc3b9deb313e39d0adbd422a08e2515bf4
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2022-10-14 13:06:57 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,7 +6,7 @@ const btnStart = document.querySelector("[data-start]");
 const days = document.querySelector("[data-days]");
 const hours = document.querySelector("[data-hours]");
 const minutes = document.querySelector("[data-minutes]");
-const seconds = document.querySelector("[data-second]");
+const seconds = document.querySelector("[data-seconds]");
 
 btnStart.disabled = true;
 
@@ -34,10 +34,10 @@ const options = {
     const hour = minute * 60;
     const day = hour * 24;
   
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    const days = addLeadingZero(Math.floor(ms / day));
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
   
     return { days, hours, minutes, seconds };
   }
@@ -49,17 +49,17 @@ let timerId = null;
 const startTimer = () => {
   btnStart.disabled = true;
   timerId = setInterval(() => {
-    let deltaTime = dateSet - currentTime;
+    let deltaTime = dateSet - Date.now();
     const convertTime = convertMs(deltaTime);
     if (deltaTime = 0) {
       clearInterval(timerId);
     } else {
       clockTimer(convertTime);
     }
-  },1000);
+  }, 1000);
 }
 
-const clockTimer = (convertTime) => {
+function clockTimer(convertTime) {
   days.textContent = convertTime.days;
   hours.textContent = convertTime.hours;
   minutes.textContent = convertTime.minutes;
@@ -68,13 +68,6 @@ const clockTimer = (convertTime) => {
 
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
-};
-
-function updateClock(value) {
-  days.innerHTML = value.days;
-  hours.innerHTML = value.hours;
-  minutes.innerHTML = value.minutes;
-  seconds.innerHTML = value.seconds;
 };
 
 btnStart.addEventListener("click", startTimer);
